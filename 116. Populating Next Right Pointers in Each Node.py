3deae2037001e9053d1acd005e8a1bedd3d3bb77
Rishabh <24757929+rishabh-kr-jain@users.noreply.github.com>
2025-02-28 11:04:28 -0800

    Update 116. Populating Next Right Pointers in Each Node.py

--- 116. Populating Next Right Pointers in Each Node.py
+++ 116. Populating Next Right Pointers in Each Node.py
@@ -29,4 +29,35 @@ class Solution:
                     continue
                 curr.next = q[0]
         return root 
-        
+
+
+#using inorder dfs
+#time: O (n)
+# space: O(1)
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if root is None:
+            return
+        self.dfs(root)
+        return root
+    def dfs(self,root):
+        if root is None:
+            return root
+        curr= root
+        if curr.left is not None:
+            curr.left.next = curr.right
+        if curr.next is not None and curr.right is not None:
+            curr.right.next= curr.next.left
+        self.dfs(curr.left)
+        self.dfs(curr.right)
+        return root 
